50097159de3d736210b2b6be54dde40bb5e4a83d
Tech-Nayuta <ketanayutawork@gmail.com>
2021-04-02 19:34:52 +0900

    Fluxに必要なファイルを生成

---
+++ CtaReactiveMaster/Sources/Flux/NewsRepository/NewsRepositoryActionCreator.swift
@@ -0,0 +1,11 @@
+//
+//  NewsRepositoryActionCreator.swift
+//  CtaReactiveMaster
+//
+//  Created by 化田晃平 on R 3/04/02.
+//
+
+import Foundation
+
+//final class NewsRepositoryActionCreator {
+//}
---
+++ CtaReactiveMaster/Sources/Flux/NewsRepository/NewsRepositoryStore.swift
@@ -0,0 +1,8 @@
+//
+//  NewsRepositoryStore.swift
+//  CtaReactiveMaster
+//
+//  Created by 化田晃平 on R 3/04/02.
+//
+
+import Foundation
