c88a537ee0ddc087e3fd72595e42ea4046f87cde
Tech-Nayuta <ketanayutawork@gmail.com>
2021-05-24 16:53:57 +0900

    add searchNews in NewsRepositoryMock

--- CtaReactiveMasterTests/Mock/NewsRepositoryMock.swift
+++ CtaReactiveMasterTests/Mock/NewsRepositoryMock.swift
@@ -10,6 +10,7 @@ import Foundation
 import RxSwift
 
 struct NewsRepositoryMock: NewsRepository {
+
     let apiClient: APIClient
 
     init(apiClient: APIClient = .init(decoder: .iso8601)) {
@@ -17,6 +18,14 @@ struct NewsRepositoryMock: NewsRepository {
     }
 
     func fetchNews() -> Single<News> {
+        createMockNews()
+    }
+
+    func searchNews(_ keyword: String) -> Single<News> {
+        createMockNews()
+    }
+
+    private func createMockNews() -> Single<News> {
         Single<News>.create(subscribe: { observer in
             print(News.mock)
             observer(.success(.mock))
